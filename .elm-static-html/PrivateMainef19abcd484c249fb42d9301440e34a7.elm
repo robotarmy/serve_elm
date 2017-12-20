ac4ef4eabade363b124a2728588892fee7d487f5
Curtis J Schofield <o_o@r913.attlocal.net>
2017-12-19 21:31:28 -0800

    more elm static cache files

---
+++ .elm-static-html/PrivateMainef19abcd484c249fb42d9301440e34a7.elm
@@ -0,0 +1,41 @@
+
+port module PrivateMainef19abcd484c249fb42d9301440e34a7 exposing (..)
+
+import Platform
+import Html exposing (Html)
+import ElmHtml.InternalTypes exposing (decodeElmHtml)
+import ElmHtml.ToString exposing (nodeToStringWithOptions, defaultFormatOptions)
+import Json.Decode as Json
+import Native.Jsonify
+
+import StaticMain
+import StaticMain
+
+
+asJsonView : Html msg -> Json.Value
+asJsonView = Native.Jsonify.stringify
+
+options = { defaultFormatOptions | newLines = True, indent = 4 }
+
+decode : Html msg -> String
+decode view =
+    case Json.decodeValue decodeElmHtml (asJsonView view) of
+        Err str -> "ERROR:" ++ str
+        Ok str -> nodeToStringWithOptions options str
+
+
+init : Json.Value -> ((), Cmd msg)
+init values =
+    case Json.decodeValue StaticMain.decodeModel values of
+        Err err -> ((), htmlOutef19abcd484c249fb42d9301440e34a7 ("ERROR:" ++ err))
+        Ok model ->
+            ((), htmlOutef19abcd484c249fb42d9301440e34a7 <| decode <| StaticMain.view model)
+
+
+main = Platform.programWithFlags
+    { init = init
+    , update = (\_ b -> (b, Cmd.none))
+    , subscriptions = (\_ -> Sub.none)
+    }
+
+port htmlOutef19abcd484c249fb42d9301440e34a7 : String -> Cmd msg
