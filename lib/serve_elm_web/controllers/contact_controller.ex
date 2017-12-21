f0244f1801e329787dc029592e0b98bcc2970001
Curtis Schofield <curtis.schofield@indiegogo.com>
2017-12-20 16:19:26 -0800

    added show api for elm client after it loads

--- lib/serve_elm_web/controllers/contact_controller.ex
+++ lib/serve_elm_web/controllers/contact_controller.ex
@@ -9,5 +9,21 @@ defmodule ServeElmWeb.ContactController do
 
     json conn, contacts
   end
+
+  def show(conn, %{"id" => id}) do
+    contact = %{
+      id: 1311,
+      first_name: "Hardcoded Api",
+      last_name: "Responder",
+      gender: 3,
+      birth_date: "11/20/1983",
+      location: "Elixir Server",
+      phone_number: "444-55-3331",
+      email: "boo@faker.com",
+      headline: "Functional Programmer",
+      picture: "https://t6.rbxcdn.com/cd18c724efadc4e7a3220c2f098b6420"
+    }
+    json conn, contact
+  end
 end
 
