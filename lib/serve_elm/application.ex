f57fb680ba153e823603619d8277a6027e1f2fce
Curtis J Schofield <o_o@r913.attlocal.net>
2017-12-20 23:27:21 -0800

    prescience.. presence

--- lib/serve_elm/application.ex
+++ lib/serve_elm/application.ex
@@ -12,6 +12,8 @@ defmodule ServeElm.Application do
       supervisor(ServeElmWeb.Endpoint, []),
       # Start your own worker by calling: ServeElm.Worker.start_link(arg1, arg2, arg3)
       worker(ServeElmWeb.Worker, []),
+      supervisor(ServeElm.Presence, []),
+
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
