dcc532d1cff0fbfdc01fc380352760bcaa5a7b68
Curtis J Schofield <o_o@r913.attlocal.net>
2017-12-23 15:04:10 -0800

    still looking into ways to streamline asset delivery

--- lib/serve_elm_web/views/layout_view.ex
+++ lib/serve_elm_web/views/layout_view.ex
@@ -1,3 +1,17 @@
 defmodule ServeElmWeb.LayoutView do
   use ServeElmWeb, :view
+  def inline_script(conn, path) do
+    path = Path.expand(
+      "../../../priv/static/#{path}",
+      __DIR__
+    )
+    content_tag(:script, "", src: data_url(
+        "application/javascript",
+        File.read!(path)
+      )
+    )
+  end
+  def data_url(mimetype,content) do
+    "data:#{mimetype};base64,#{Base.encode64(content)}"
+  end
 end
